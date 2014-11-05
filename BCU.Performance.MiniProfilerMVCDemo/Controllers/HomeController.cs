fab48aeb53036b95d769b564e9a27d9fd4a03312
bcutajar <bc198990@gmail.com>
2014-11-05 14:43:26 +0100

    BCU -First Commit

---
+++ BCU.Performance.MiniProfilerMVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using StackExchange.Profiling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BCU.Performance.MiniProfilerMVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MiniProfiler profiler = MiniProfiler.Current;
+
+            using (profiler.Step("Loading Homepage View"))
+            {
+                using (profiler.Step("Doing Complex Calculations"))
+                {
+                    Thread.Sleep(300);
+                }
+
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BCU.Performance.MiniProfilerMVCDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BCU.Performance.MiniProfilerMVCDemo.MvcApplication" Language="C#" %>
